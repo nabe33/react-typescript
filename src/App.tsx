935858d201bd4927316dffc012b850992339e14d
Takayuki Watanabe <nabe@lab.twcu.ac.jp>
2023-04-30 16:44:38 +0900

    finish intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-const App = () => {
-  return <div>React Starter Kit in TypeScript</div>;
+interface AppProps {
+  message: string;
+}
+
+const App = ({ message }: AppProps) => {
+  // console.log(props);
+  return <div>{message}</div>;
 };
 
 export default App;
